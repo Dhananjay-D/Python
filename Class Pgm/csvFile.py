92363c24fb65de5f894e61ede9876863a5ff083b
Dhananjay Dharne <dsdharne2019@gmail.com>
2023-07-22 20:27:15 +0530

    deleted unwanted files

---
+++ Class Pgm/csvFile.py
@@ -0,0 +1,14 @@
+# importing CSV module :
+import csv
+
+with open("CSVFILE.csv","r") as csv_file:
+    # creating Reader obj to read from the csv_file
+    Reader=csv.reader(csv_file)
+    for line in Reader:
+        print(line[0]," ",line[1]," ",line[2])
+
+    with open("CSVFILE2.csv","w") as csv_file:
+        # creating Reader obj to read from the csv_file
+        Writer=csv.writer(csv_file)
+        for line in Reader:
+            Writer.writerow(line)
